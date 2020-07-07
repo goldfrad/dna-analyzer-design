20dbc3bcbe01f6181ffa9da9f4c12b3acc6ce6bc
yehudit goldfrad <z618149983@gmail.com>
2020-07-06 17:05:08 -0700

    exit part of input

--- SRC/factoryCommand.cpp
+++ SRC/factoryCommand.cpp
@@ -1,13 +1,9 @@
 
 #include "factoryCommand.h"
 
-static ICommand* FactoryCommand::getCommand(std::string& lineOfCommand) {
-    std::vector<std::string> array;
-    std::stringstream ss(lineOfCommand);
-    std::string temp;
-    while (ss >> temp)
-        array.push_back(temp);
-    if (!strcmp("new", array[0].c_str())){
+ICommand* FactoryCommand::getCommand(std::string& command) {
+
+    if (!strcmp("new", command.c_str())){
         ICommand* n = new New;
         return n;
     }
