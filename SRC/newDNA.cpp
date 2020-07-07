a38d01e27d4b8e0a32ca686e4cde97dbc1825c5a
yehudit goldfrad <z618149983@gmail.com>
2020-07-07 16:37:43 -0700

    use in insert

--- SRC/newDNA.cpp
+++ SRC/newDNA.cpp
@@ -1,8 +1,9 @@
 #include "newDNA.h"
 
 int New::run(std::vector<std::string>& vecParmeter) {
-    Data newdna(vecParmeter[1]);
-    std::cout << "[" << newdna.getID() << "] " << newdna.getDNA() << std::endl;
+    Data newdna(vecParmeter[1], vecParmeter[2].erase(0, 1));
+    m_ds.insert(newdna);
+    std::cout << "[" << newdna.getID() << "] " << newdna.getDNA() << " " << newdna.getName() << std::endl;
     return 1;
 }
 
